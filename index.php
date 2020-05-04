4cca5c012e006bb084a751255f8678722e15d4eb
Stefan Stafie <stefan_stafie@yahoo.com>
2020-05-05 00:40:56 +0300

    Signed-off-by: Stefan Stafie <stefan_stafie@yahoo.com>

--- index.php
+++ index.php
@@ -1,11 +1,6 @@
 
 <?php
 session_start();
-echo $_SESSION['user_id'];
-if(isset($_SESSION['user_id'])){
-    echo "you are in";
-    echo "<a href = \"Resources/db/signout.php\">sign out</a>"; 
-} else{
-    echo "<a href = \"Resources/db/register.php\">Register</a> or <a href = \"Resources/db/login.php\">Login</a>";
-}
+
 ?>
+this should be a page handler
